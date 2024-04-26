e61a38731f56fbbf5982bcd007c794f767de817e
luska <luskakosterina5811@gmail.com>
2024-04-26 14:02:28 +0300

    fin

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Reshi_prim"
+include(":app")
+ 
